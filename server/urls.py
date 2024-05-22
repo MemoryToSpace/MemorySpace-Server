d9e1166a1b6fd745c698bba56142be0dd0b3a7b5
Koren <korenkpalan96@gmail.com>
2024-05-22 15:19:18 +0300

    deployment

--- server/urls.py
+++ server/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),  # Include your app's URL patterns
 ]
+
+urlpatterns += staticfiles_urlpatterns()
