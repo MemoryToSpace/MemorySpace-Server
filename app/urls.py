bed6a898d06730d10a67451ada719f43a5f560eb
MemoryToSpace <memorytospace@gmail.com>
2024-05-23 12:47:54 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('generate_test/', views.generate_test, name='generate_test'),
     path('generate/', views.generate_image, name='generate'),
+    path('generate_image/', views.generate_image_response, name='generate_image_response'),
     # Add more URL patterns as needed
 ]
